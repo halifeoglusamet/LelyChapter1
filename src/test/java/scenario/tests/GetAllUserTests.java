ca3a5fe600f0770c82af18788e0af3cc81ab13cc
shalifeo <shalifeo@cisco.com>
2023-05-18 19:18:04 +0300

    added id check

--- src/test/java/scenario/tests/GetAllUserTests.java
+++ src/test/java/scenario/tests/GetAllUserTests.java
@@ -1,18 +1,45 @@
 package scenario.tests;
 
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 import scenario.base.BaseTest;
 
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertTrue;
 
 public class GetAllUserTests extends BaseTest {
 
+    private final String PATH = "/users ";
+    JSONObject jsonResponse;
+    JSONArray dataArray;
+
     @Test
     public void getAllBookingTest() {
-        given(spec)
+        Response response = given(spec)
                 .when()
-                .get("/users")
+                .get(PATH);
+
+        response
                 .then()
                 .statusCode(200);
+
+        var responseBody = response.getBody().asString();
+
+        jsonResponse = new JSONObject(responseBody);
+        dataArray = jsonResponse.getJSONArray("data");
+
+        boolean allIdsAre7Digits = true;
+
+        for (int i = 0; i < dataArray.length(); i++) {
+            var user = dataArray.getJSONObject(i);
+            var id = user.getInt("id");
+            if (String.valueOf(id).length() != 7) {
+                allIdsAre7Digits = false;
+                break;
+            }
+        }
+        assertTrue(allIdsAre7Digits, "Not all data.id values are 7-digit integers.");
     }
 }
